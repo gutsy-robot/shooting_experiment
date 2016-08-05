4e4735c54e536fc2b5c48b2c0b2dbb75be0e80e7
Sid <agrawalsiddharth12@gmail.com>
2016-08-05 16:58:01 +1000

    Woah

--- py_main.py
+++ py_main.py
@@ -5,14 +5,7 @@ import time
 def main():
   
 
-  rs = RobotScript()
-
-  channel_speech_id = rs.addChannel("say")
-
-  asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
-  asset_imchip_id = rs.addAssetToChannel(channel_speech_id, [4], ("I am Gutsy!",)
-  rs.play()
-
+  PyPR2.say("Welcome")
 
 
 
