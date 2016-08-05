c51315a99ac278b40fc5952fcde2653a527b3585
Sid <agrawalsiddharth12@gmail.com>
2016-08-05 16:03:31 +1000

    Woahgit commit

--- py_main.py
+++ py_main.py
@@ -2,13 +2,13 @@ from SIMKit import RobotScript, Event
 import PyPR2
 
 def main():
-rs = RobotScript()
-channel_playmotion_id = rs.addChannel("playDefaultMotion", "onPlayMotionSuccess", "onPlayMotionFailed")
-channel_speech_id = rs.addChannel("say", "onSpeakSuccess", "onSpeakFailed")
-asset_wave_id = rs.addAssetToChannel(channel_playmotion_id, [0], ("wave",))
-asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
-asset_imchip_id = rs.addAssetToChannel(channel_speech_id, [4], ("I am REEM!",)
-rs.play()
+  rs = RobotScript()
+
+  channel_speech_id = rs.addChannel("say", "onSpeakSuccess", "onSpeakFailed")
+
+  asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
+  asset_imchip_id = rs.addAssetToChannel(channel_speech_id, [4], ("I am Gutsy!",)
+  rs.play()
 
 
 
