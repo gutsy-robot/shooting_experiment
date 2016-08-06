a5c9209fd1677d11606cb0654a785be4a606c034
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 20:30:56 +1000

    first change in running

--- commands.py
+++ commands.py
@@ -6,9 +6,5 @@ def generate():
 	
 	rs = RobotScript()
 	channel_torso_id = rs.addChannel("moveTorsoBy")
-	channel_head_id = rs.addChannel("moveHeadTo")
-	asset_torso_id = rs.addAssetToChannel(channel_torso_id, [3], (-0.03,10))
-	asset_head_id = rs.addAssetToChannel(channel_head_id, [14], (2.0,0.2))
+	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (-0.03,10))
 	return rs
-
- 
