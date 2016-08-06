7bfcaf9c00fc2f1f82a1ee1c8777dff9e3c990bb
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 19:07:04 +1000

    Woah

--- commands.py
+++ commands.py
@@ -7,6 +7,6 @@ def generate():
 	channel_speech_id = rs.addChannel("say")
 	channel_torso_id = rs.addChannel("moveTorsoBy")
 	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
-	asset_torso_id = rs.addAssetToChannel(channel_torso_id, [5], (0.2,7))
+	asset_Sid_id = rs.addAssetToChannel(channel_speech_id, [7], ("Hi Sid!",))
 	rs.play()
 		
