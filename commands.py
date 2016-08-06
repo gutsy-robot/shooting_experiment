8261dde71f696eedfcf05f1b159860484ee4a4ce
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 19:09:04 +1000

    Woah

--- commands.py
+++ commands.py
@@ -5,8 +5,7 @@ import PyPR2
 def generate():
 	rs = RobotScript()
 	channel_speech_id = rs.addChannel("say")
-	channel_torso_id = rs.addChannel("moveTorsoBy")
+	
 	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
-	asset_Sid_id = rs.addAssetToChannel(channel_speech_id, [7], ("Hi Sid!",))
 	rs.play()
 		
