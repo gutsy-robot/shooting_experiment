9265249ce426766fd093e83207c08b58747a2e48
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 20:16:56 +1000

    Testing

--- commands.py
+++ commands.py
@@ -2,11 +2,11 @@ from SIMKit import RobotScript, Event
 import PyPR2
 
 
-def generate(rs):
+def generate():
 	
-	
+	rs = RobotScript()
 	channel_torso_id = rs.addChannel("moveTorsoBy")
 	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (-0.03,10))
-	rs.play()
+	return rs
 
-
+ 
