f5c3562d5eb9a4f3675091b9ab753e4eeaf07220
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 19:35:55 +1000

    woa

--- commands.py
+++ commands.py
@@ -3,10 +3,10 @@ import PyPR2
 
 
 def generate():
-#	rs = RobotScript()
-##	channel_speech_id = rs.addChannel("say")
-#	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, #[2], ("Hello world!",))
-#	rs.play()
+	rs = RobotScript()
+	channel_speech_id = rs.addChannel("say")
+	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hello world!",))
+	rs.play()
 
 	PyPR2.say("Hi")
 	PyPR2.moveTorsoBy(0.2,10)
