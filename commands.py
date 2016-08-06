23c6179428034f11ab7680abc396bb47974e3198
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 17:02:10 +1000

    Woah

--- commands.py
+++ commands.py
@@ -1,7 +1,14 @@
-import PyPR2
 from SIMKit import RobotScript, Event
+import PyPR2
 
 
 class commands:
-	def A(self):
-		PyPR2.say("Hi")
+	def __init__(self,sc):
+		self.script= sc
+	
+
+	def play(self):
+		self.script.play()
+
+
+		
