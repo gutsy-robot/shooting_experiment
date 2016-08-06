430ef28f67002155d26fb4696fd921eb0c5b1aed
Sid <agrawalsiddharth12@gmail.com>
2016-08-06 19:55:10 +1000

    Testing

--- commands.py
+++ commands.py
@@ -5,8 +5,8 @@ import PyPR2
 def generate():
 	
 	rs = RobotScript()
-	channel_speech_id = rs.addChannel("say")
-	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [5], ("Hello world!",))
+	channel_torso_id = rs.addChannel("moveTorsoBy")
+	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (-0.03,10))
 	rs.play()
 
 
