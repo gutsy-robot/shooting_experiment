7ad7d88997e75603c45ee62d53dd23f6e58a8e3c
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 11:56:13 +1000

    random

--- commands.py
+++ commands.py
@@ -6,9 +6,10 @@ import PyPR2
 def generate():
 	
 	
-	rs = RobotScript()
-	channel_speech_id = rs.addChannel("say")
-	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hi There!"))
-	rs.play()
+##	rs = RobotScript()
+##	channel_speech_id = rs.addChannel("say")
 
+##	asset_helloworld_id = rs.addAssetToChannel(channel_speech_id, [2], ("Hi There!"))
+##	rs.play()
 
+	PyPR2.say("hi")
