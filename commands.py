86402a7436d67b7f8634c98799da6067318ca512
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 11:29:46 +1000

    Without RobotScript

--- commands.py
+++ commands.py
@@ -4,7 +4,10 @@ import PyPR2
 
 def generate():
 	
-	rs = RobotScript()
-	channel_torso_id = rs.addChannel("moveTorsoBy")
-	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (0.06,10))
-	return rs
+###	rs = RobotScript()
+
+##	channel_torso_id = rs.addChannel("moveTorsoBy")
+##	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (0.06,10))
+##	return rs
+
+	PyPR2.say("Hello There!!")
