7f2b08b294260ec4ba19487db926be0ed7c18139
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 14:27:14 +1000

    new additions after success

--- commands.py
+++ commands.py
@@ -9,7 +9,7 @@ def generate():
 	rs = RobotScript()
 	channel_head_id = rs.addChannel("moveHeadTo")
 	channel_tuck_id = rs.addChannel("tuckBothArms")
-	channel_speech_id = rs.addChanel("say")
+	channel_speech_id = rs.addChannel("say")
 	asset_tuck_id = rs.addAssetToChannel(channel_tuck_id, [2], ())
 	asset_speech_id = rs.addAssetToChannel(channel_speech_id, [2], ("Initial lising"))
 	asset_head_id = rs.addAssetToChannel(channel_head_id, [2], (2.0,0.0))
