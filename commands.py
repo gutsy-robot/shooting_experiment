fb7904fc42a120ab7281d342e85d9293498d5206
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 11:40:28 +1000

    create_obj

--- commands.py
+++ commands.py
@@ -2,16 +2,17 @@ from SIMKit import RobotScript, Event
 import PyPR2
 
 
-global rs
-
-rs = RobotScript()
 
 def generate():
 	
-	
+	rs = create_obj()
 
 	channel_torso_id = rs.addChannel("moveTorsoBy")
 	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (0.06,10))
 	return rs
 
+
+def create_obj():
+	a = RobotScript()
+	return a
 	
