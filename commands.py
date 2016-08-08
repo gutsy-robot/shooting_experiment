8e73c1ff1cd90c0acf6019b1f6834ebe276c6ef4
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 14:29:37 +1000

    new additions after success

--- commands.py
+++ commands.py
@@ -11,7 +11,7 @@ def generate():
 	channel_tuck_id = rs.addChannel("tuckBothArms")
 	channel_speech_id = rs.addChannel("say")
 	asset_tuck_id = rs.addAssetToChannel(channel_tuck_id, [2], ())
-	asset_speech_id = rs.addAssetToChannel(channel_speech_id, [2], ("Initial lising"))
+	asset_speech_id = rs.addAssetToChannel(channel_speech_id, [2], ("Initial lising",))
 	asset_head_id = rs.addAssetToChannel(channel_head_id, [2], (2.0,0.0))
 	rs.play()
 
