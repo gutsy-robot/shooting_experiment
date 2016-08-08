ad673943693f719d029535917b9fb17adb875a01
Sid <agrawalsiddharth12@gmail.com>
2016-08-08 11:00:48 +1000

    Changing Value

--- commands.py
+++ commands.py
@@ -6,5 +6,5 @@ def generate():
 	
 	rs = RobotScript()
 	channel_torso_id = rs.addChannel("moveTorsoBy")
-	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (0.03,10))
+	asset_helloworld_id = rs.addAssetToChannel(channel_torso_id, [5], (0.06,10))
 	return rs
