2e614a48b96031163db3c52e39a31fd46f55bef7
Sid <agrawalsiddharth12@gmail.com>
2016-08-11 11:23:35 +1000

    Fun Time

--- commands.py
+++ commands.py
@@ -31,7 +31,7 @@ def generate():
 	asset_hands_id2 = rs.addAssetToChannel(channel_hands_id, [4],({'l_wrist_roll_joint': -0.0007893761161641422, 'l_forearm_roll_joint': -5.784708695053728e-05, 'l_elbow_flex_joint': -0.1483631860063741, 'l_shoulder_lift_joint': 0.356638539879416, 'l_upper_arm_roll_joint': 0.15967385473500117, 'l_wrist_flex_joint': -0.07863929718151064, 'l_shoulder_pan_joint': 0.0035887617206241673}))
 	asset_hands_id3 = rs.addAssetToChannel(channel_hands_id, [6], ({'l_wrist_roll_joint': 3.0377253864391696, 'l_forearm_roll_joint': 3.0636974190755852, 'l_elbow_flex_joint': -2.045293817264738, 'l_shoulder_lift_joint': 0.7999295928757233, 'l_upper_arm_roll_joint': 0.11734018057335116, 'l_wrist_flex_joint': -0.6073161853116821, 'l_shoulder_pan_joint': -0.024019141859252136}))
 	asset_hands_id4 = rs.addAssetToChannel(channel_hands_id, [8], ({'r_elbow_flex_joint': -0.6838700474140432, 'r_shoulder_lift_joint': -0.2795862208602225, 'r_upper_arm_roll_joint': -0.7924339995905723, 'r_wrist_roll_joint': -0.2403805679846016, 'r_shoulder_pan_joint': -2.37771742738202e-05, 'r_forearm_roll_joint': 5.099914879735316, 'r_wrist_flex_joint': -1.7826226439620259}))
-	asset_speech_id2 = rs.addAssetToChannel(channel_speech_id, [13], ("Will Kill you,   Soon",))
+	#asset_speech_id2 = rs.addAssetToChannel(channel_speech_id, [13], ("Will Kill you,   Soon",))
 	asset_head_id1 = rs.addAssetToChannel(channel_head_id, [10], (2.0,0.5))
 	asset_head_id2 = rs.addAssetToChannel(channel_head_id, [13], (0.0,0.0))
 	rs.play()
