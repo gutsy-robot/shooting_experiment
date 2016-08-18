e9b9b7a3f1b405b3dfc15abe2d6441d54c41355b
Sid <agrawalsiddharth12@gmail.com>
2016-08-18 18:05:22 +1000

    ROS NODE

--- py_main.py
+++ py_main.py
@@ -3,6 +3,10 @@ import PyPR2
 import time
 import commands
 import iksresolver
+import rospy
+from std_msgs.msg import String
+
+
 
 iksResolver = None
 
@@ -13,3 +17,12 @@ def main():
   
 	global iksResolver
   	iksResolver = iksresolver.IKSResolver()
+
+	pub = rospy.Publisher('chatter', String, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+          hello_str = "hello world %s" % rospy.get_time()
+          rospy.loginfo(hello_str)
+          pub.publish(hello_str)
+          rate.sleep()
