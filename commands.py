750d62b8981545f3cde2af17cee7bc02c5b44921
Sid <agrawalsiddharth12@gmail.com>
2016-09-01 21:58:03 +1000

    Fix Magiks error

--- commands.py
+++ commands.py
@@ -98,8 +98,40 @@ def onHumanTracking(tracking_objs):
 	focus_obj = tracking_objs[0]
 	if focus_obj['est_pos'][0]<=4 and focus_obj['est_pos'][0] >3:
 		PyPR2.moveArmWithJointPos(**left_shooting)
+		mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
+      		mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
+      		#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
+      		ofs_x = mid_x - 320
+      		ofs_y = mid_y - 240
+      		chx = chy = 0.0
+      		if math.fabs(ofs_x) > 10:
+       			chx = -ofs_x * 90.0 / 640 * 0.01745329252
+      		if math.fabs(ofs_y) > 10:
+        		chy = ofs_y * 90.0 / 640 * 0.01745329252
+      		PyPR2.updateHeadPos( chx, chy )
+		if chx==initial_x and chy ==initial_y:
+			PyPR2.say("station ary")
+		else:	
+			initial_x = chx
+			initial_y =chy
 	elif focus_obj['est_pos'][0]<=3 and focus_obj['est_pos'][0] >2:
 		PyPR2.moveArmWithJointPos(**right_shooting)
+		mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
+      		mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
+      		#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
+      		ofs_x = mid_x - 320
+      		ofs_y = mid_y - 240
+      		chx = chy = 0.0
+      		if math.fabs(ofs_x) > 10:
+       			chx = -ofs_x * 90.0 / 640 * 0.01745329252
+      		if math.fabs(ofs_y) > 10:
+        		chy = ofs_y * 90.0 / 640 * 0.01745329252
+      		PyPR2.updateHeadPos( chx, chy )
+		if chx==initial_x and chy ==initial_y:
+			PyPR2.say("station ary")
+		else:	
+			initial_x = chx
+			initial_y =chy
 		
 	else:
       		mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
