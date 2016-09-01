06187d542ed891eac5ea3d476d16d6875a4f4cd3
Sid <agrawalsiddharth12@gmail.com>
2016-09-02 01:40:04 +1000

    Fix Magiks error

--- commands.py
+++ commands.py
@@ -109,13 +109,12 @@ def onHumanTracking(tracking_objs):
       		if math.fabs(ofs_y) > 10:
         		chy = ofs_y * 90.0 / 640 * 0.01745329252
       			PyPR2.updateHeadPos( chx, chy )
-			if abs(chx-initial_x) <= 1 and abs(chy -initial_y)<=1:
+			if abs(chx-initial_x) <= 0.11 and abs(chy -initial_y)<=0.11:
 				PyPR2.tuckBothArms()
 			else:	
 
 				PyPR2.moveTorsoBy(0.1,6)
-				initial_x = chx
-				initial_y =chy
+
 	elif focus_obj['est_pos'][0]<=3 and focus_obj['est_pos'][0] >2:
 		PyPR2.moveArmWithJointPos(**right_shooting)
 		mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
@@ -129,12 +128,11 @@ def onHumanTracking(tracking_objs):
       		if math.fabs(ofs_y) > 10:
         		chy = ofs_y * 90.0 / 640 * 0.01745329252
       			PyPR2.updateHeadPos( chx, chy )
-		        if abs(chx-initial_x) <= 1 and abs(chy -initial_y)<=1:
+		        if abs(chx-initial_x) <= 0.11 and abs(chy -initial_y)<=0.11:
 				PyPR2.tuckBothArms()
 			else:	
 				PyPR2.moveTorsoBy(0.1,6)
-				initial_x = chx
-				initial_y = chy
+				
 		
 	else:
 		
@@ -149,13 +147,11 @@ def onHumanTracking(tracking_objs):
       		if math.fabs(ofs_y) > 10:
         		chy = ofs_y * 90.0 / 640 * 0.01745329252
       			PyPR2.updateHeadPos( chx, chy )
-			if abs(chx-initial_x) <= 1 and abs(chy -initial_y)<=1:
+			if abs(chx-initial_x) <= 0.11 and abs(chy -initial_y)<=0.11:
 				PyPR2.tuckBothArms()
 			else:	
 
 				PyPR2.moveTorsoBy(0.1,6)			
-				initial_x = chx
-				initial_y =chy
 
 
 
