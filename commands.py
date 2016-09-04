f3788530e57d9168d0cf28f1d0e67d3426a8dc94
Sid <agrawalsiddharth12@gmail.com>
2016-09-04 14:32:36 +1000

    Reaction Tracking

--- commands.py
+++ commands.py
@@ -39,7 +39,7 @@ right_up = {'r_elbow_flex_joint': -2.0722211695820745, 'r_shoulder_lift_joint': 
 
 
 
-previous_pos = 0
+#previous_pos = 0
 
 
 def arm_back():
@@ -113,7 +113,7 @@ def onHumanTracking(tracking_objs):
       			if math.fabs(ofs_y) > 10:
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
-				previous_pos = focus_obj['est_pos'][0]
+				#previous_pos = focus_obj['est_pos'][0]
 			
 
 	elif focus_obj['est_pos'][0]<=3 and focus_obj['est_pos'][0] >2:
@@ -129,7 +129,7 @@ def onHumanTracking(tracking_objs):
       			if math.fabs(ofs_y) > 10:
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
-				previous_pos = focus_obj['est_pos'][0]		        			
+				#previous_pos = focus_obj['est_pos'][0]		        			
 		
 	elif focus_obj['est_pos'][0] <2:
 			PyPR2.moveArmWithJointPos(**right_pullback_alt)		
@@ -144,7 +144,19 @@ def onHumanTracking(tracking_objs):
       			if math.fabs(ofs_y) > 10:
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
-				previous_pos = focus_obj['est_pos'][0]				
+				#previous_pos = focus_obj['est_pos'][0]	
+	else:
+			mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
+      			mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
+      			#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
+      			ofs_x = mid_x - 320
+      			ofs_y = mid_y - 240
+      			chx = chy = 0.0
+      			if math.fabs(ofs_x) > 10:
+       				chx = -ofs_x * 90.0 / 640 * 0.01745329252
+      			if math.fabs(ofs_y) > 10:
+        			chy = ofs_y * 90.0 / 640 * 0.01745329252
+      				PyPR2.updateHeadPos( chx, chy )			
 
 	   
 
