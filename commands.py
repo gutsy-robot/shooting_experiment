274466348f7b1688d8264cb6178f9f8a45a000f9
Sid <agrawalsiddharth12@gmail.com>
2016-09-04 14:40:35 +1000

    Reaction Tracking

--- commands.py
+++ commands.py
@@ -101,7 +101,7 @@ def onHumanTracking(tracking_objs):
 	#	PyPR2.moveHeadTo(0.2,1.0)
 	
 	if focus_obj['est_pos'][0]<=4 and focus_obj['est_pos'][0] >3:
-			PyPR2.moveArmWithJointPos(**left_shooting)
+			
 			mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
       			mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
       			#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
@@ -114,10 +114,10 @@ def onHumanTracking(tracking_objs):
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
 				#previous_pos = focus_obj['est_pos'][0]
-			
+			PyPR2.moveArmWithJointPos(**left_shooting)
 
 	elif focus_obj['est_pos'][0]<=3 and focus_obj['est_pos'][0] >2:
-			PyPR2.moveArmWithJointPos(**right_shooting)
+			
 			mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
       			mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
       			#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
@@ -129,10 +129,11 @@ def onHumanTracking(tracking_objs):
       			if math.fabs(ofs_y) > 10:
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
+			PyPR2.moveArmWithJointPos(**right_shooting)
 				#previous_pos = focus_obj['est_pos'][0]		        			
 		
 	elif focus_obj['est_pos'][0] <2:
-			PyPR2.moveArmWithJointPos(**right_pullback_alt)		
+				
       			mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
       			mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
       			#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
@@ -144,6 +145,7 @@ def onHumanTracking(tracking_objs):
       			if math.fabs(ofs_y) > 10:
         			chy = ofs_y * 90.0 / 640 * 0.01745329252
       				PyPR2.updateHeadPos( chx, chy )
+			PyPR2.moveArmWithJointPos(**right_pullback_alt)	
 				#previous_pos = focus_obj['est_pos'][0]	
 	else:
 			mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
