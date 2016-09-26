644860a7020293d26516e36d48926c0d948b7730
Sid <agrawalsiddharth12@gmail.com>
2016-09-26 10:19:53 +1000

    Track everything

--- commands.py
+++ commands.py
@@ -83,9 +83,8 @@ revolve_counter= 1
 torso_position_counter = 0
 last_action_counter = 0
 sub_action_flag =1 
-csvfile1 = "/home/demoshare/shooting_experiment/test5.csv"
-csvfile2 = "/home/demoshare/shooting_experiment/test6.csv"
-csvfile3 = "/home/demoshare/shooting_experiment/test7.csv"
+csvfile = "/home/demoshare/shooting_experiment/test8.csv"
+
 
 
 def revolve_cw():
@@ -214,19 +213,14 @@ def onHumanTracking(tracking_objs):
       		PyPR2.updateHeadPos( chx, chy )
 	adjust_to_shooting(chy)
 	elapsed_time = time.time() - start_time
-	track_x.append((focus_obj['est_pos'][0],elapsed_time))
-	track_y.append((focus_obj['est_pos'][1],elapsed_time))
-	track_d.append((d,elapsed_time,last_action_counter))
-	with open(csvfile1, "w") as output:
+	track_x.append(focus_obj['est_pos'][0])
+	track_y.append(focus_obj['est_pos'][1])
+	track_d.append((d,last_action_counter))
+	with open(csvfile, "w") as output:
    		 writer = csv.writer(output, lineterminator='\n')
-    	         writer.writerows(track_d)
+    	         writer.writerows(track_d,track_x,track_y,elapsed_time,last_action_counter)
 
-	with open(csvfile2, "w") as output:
-   		 writer = csv.writer(output, lineterminator='\n')
-    	         writer.writerows(track_x)
-	with open(csvfile3, "w") as output:
-   		 writer = csv.writer(output, lineterminator='\n')
-    	         writer.writerows(track_y)
+	
 	#PyPR2.moveTorsoBy(0.03,5)
 	#if abs(previous_pos - focus_obj['est_pos'][0])< 0.1:	
 	#	PyPR2.moveHeadTo(0.2,1.0)
