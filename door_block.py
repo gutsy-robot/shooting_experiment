f819bd3a555ff508f700076d886a968bbfc1b6f5
siddharth <siddharth@siddharth-VirtualBox.(none)>
2016-11-10 22:01:22 +1100

    nohumantracker

--- door_block.py
+++ door_block.py
@@ -25,6 +25,10 @@ sys.path.append(pyinterval_path)
 sys.path.append(mtpltlib_path)
 sys.path.append('/home/demoshare/shooting_experiment/Magiks/')
 
+HUMAN_COUNTER=0
+no_objTracker = []
+
+st_time = time.time()
 
 def onHumanDetected(objtype, nameid, trackid, status):
 	PyPR2.moveTorsoBy(0.1,2)
@@ -32,28 +36,49 @@ def onHumanDetected(objtype, nameid, trackid, status):
 
 
 def onHumanTracking(tracking_objs):
-	object_index = closest_obj_index(tracking_objs)
-	focus_obj = tracking_objs[object_index]
-	x = focus_obj['est_pos'][0]
-	y = focus_obj['est_pos'][1]
+ global HUMAN_COUNTER, st_time
+ 
+	#focus_obj = tracking_objs[object_index]
+
+	
+ if len(tracking_objs) == 0:
+		if HUMAN_COUNTER !=0:
+			st_time = time.time()
+			HUMAN_COUNTER =0
+
+		
+			
+
+ else:
+		if HUMAN_DETECTION_COUNTER ==0:
+			elapsed_time = time.time() - st_time
+			no_objTracker.append(elapsed_time)
+			HUMAN_DETECTION_COUNTER= len(tracking_objs)
+			
+
+		focus_obj = tracking_objs[object_index]
+
+
+		x = focus_obj['est_pos'][0]
+		y = focus_obj['est_pos'][1]
 
 	
 	
-	mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
+		mid_x = focus_obj['bound'][0] + focus_obj['bound'][2] / 2
       			
-	mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
-     			#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
-      	ofs_x = mid_x - 320
-      	ofs_y = mid_y - 240
-      	chx = chy = 0.0
+		mid_y = focus_obj['bound'][1] + focus_obj['bound'][3] / 2
+     				#print "track obj {} mid pt ({}.{})".format(focus_obj['track_id'],mid_x,mid_y)
+      		ofs_x = mid_x - 320
+      		ofs_y = mid_y - 240
+      		chx = chy = 0.0
 			
-      	if math.fabs(ofs_x) > 10:
-       		chx = -ofs_x * 90.0 / 640 * 0.01745329252	
+      		if math.fabs(ofs_x) > 10:
+       			chx = -ofs_x * 90.0 / 640 * 0.01745329252	
 				#head_yaw_list.append(chx)
 				
-      	if math.fabs(ofs_y) > 10:
-        	chy = ofs_y * 90.0 / 640 * 0.01745329252
-	PyPR2.updateHeadPos( chx, chy )
+      		if math.fabs(ofs_y) > 10:
+        		chy = ofs_y * 90.0 / 640 * 0.01745329252
+		PyPR2.updateHeadPos( chx, chy )
 
 
 
